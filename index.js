9c61f42cc4a8d5483613e5373504c4941b41454c
Anker Peet <ankerpeet@Ankers-Mac-mini.local>
2021-11-05 23:46:15 -0400

    updated function names

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function Dom (){
     this.DomEvents = function () {
+        this.addClass = _addClass;
+        this.removeClass = _removeClass;
+        this.css = _updateCSS;
         this.body = new DomElement(document.body);
         this.add = function(name, el) { this[name] = new DomElement(el) };
     }
@@ -10,31 +13,31 @@ function Dom (){
         self.css = function(prop, value) {
             if(self.elements.length != undefined) {
                 for(var i = 0; i < self.elements.length; i++) {  
-                    this._updateCSS(elements[i], prop, value);
+                    _updateCSS(elements[i], prop, value);
                 }
             }
             else {
-                this._updateCSS(elements, prop, value);
+                _updateCSS(elements, prop, value);
             }
         }
         self.addClass = function(className) {
             if(self.elements.length != undefined) {
                 for(var i = 0; i < self.elements.length; i++) {  
-                    this._addClass(elements[i], className);
+                    _addClass(elements[i], className);
                 }
             }
             else {
-                this._addClass(elements, className);
+                _addClass(elements, className);
             }
         }
         self.removeClass = function(className) {
             if(self.elements.length != undefined) {
                 for(var i = 0; i < self.elements.length; i++) {  
-                    this._removeClass(elements[i], className);
+                    _removeClass(elements[i], className);
                 }
             }
             else {
-                this._removeClass(elements, className);
+                _removeClass(elements, className);
             }
         }
         self.eventNames = ["mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", 
@@ -46,15 +49,15 @@ function Dom (){
         })();
     }
 
-    this._addClass = function(element, className) {
+    const _addClass = function (element, className) {
         element.classList.add(className);
     }
 
-    this._removeClass = function(element, className) {
+    const _removeClass = function(element, className) {
         element.classList.remove(className);
     }
 
-    this._updateCSS = function(element, prop, value) {
+    const _updateCSS = function(element, prop, value) {
         if(element.style[prop] != undefined) {
             element.style[prop] = value;
         }
